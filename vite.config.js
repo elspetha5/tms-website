ce1f2a6c66f08798a2c58d2f7e9d1c8ab12f5705
Elspeth Anderson <elspeth.anderson@discounttire.com>
2025-05-30 18:42:11 -0700

    took back config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/tms-website/",
-  plugins: [react()],
+export default defineConfig(() => {
+  const config = {
+    plugins: [react()],
+  };
+
+  return config;
 });
