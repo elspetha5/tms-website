53d40a1f66d7c9536619d34612df7c9168302014
Elspeth Anderson <elspeth.anderson@discounttire.com>
2025-05-30 18:08:21 -0700

    added deploy yaml

--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_NEW_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
