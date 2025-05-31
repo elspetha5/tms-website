af0af26c03efcef8bed8906eba70fdb687cccd28
Elspeth Anderson <elspeth.anderson@discounttire.com>
2025-05-30 18:46:57 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
   };
+
+  if (command === "build") {
+    config.base = "/tms-website/";
+  }
 
   return config;
 });
