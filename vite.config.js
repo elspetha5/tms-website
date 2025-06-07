3e14bb75e9ce0172f8cf13ff02b90ebe39c16a21
Elspeth Anderson <elspeth.anderson@discounttire.com>
2025-06-07 09:44:38 -0700

    removing /tms-website routing

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   const config = {
     plugins: [react()],
   };
 
-  if (command === "build") {
-    config.base = "/tms-website/";
-  }
+  // if (command === "build") {
+  //   config.base = "/tms-website/";
+  // }
 
   return config;
 });
