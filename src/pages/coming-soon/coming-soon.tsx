69f41b03b0ab239e0b3033946120b4fe1a861f01
Elspeth Anderson <elspeth.anderson@discounttire.com>
2025-07-29 12:15:33 -0700

    Updated coming soon wording

--- src/pages/coming-soon/coming-soon.tsx
+++ src/pages/coming-soon/coming-soon.tsx
@@ -4,10 +4,12 @@ import UnorderedList from "../../components/unordered-list/unordered-list";
 import "./coming-soon.scss";
 
 const comingSoonItems = [
+  { text: "Login for clients" },
   { text: "Client profiles" },
-  { text: "Accesss to invoice PDFS" },
+  { text: "Access to invoices" },
   { text: "See updates on support tickets" },
-  { text: "See your spares inventory and order devices" },
+  { text: "Device spares inventory list" },
+  { text: "Order new devices" },
   { text: "View your contract" },
   { text: "Pay online" },
   { text: "Add add-on services" },
@@ -18,13 +20,11 @@ function ComingSoon() {
   return (
     <ContentPage title="Coming Soon!">
       <div className="content-page-paragraph">
-        We're working hard to improve the level of service we believe you
-        deserve. In the next few months you can expect to see these features to
-        make your experience with us as smooth as possible:
+        We're working hard to improve the level of service you want and deserve.
+        The features below will be added shortly:
       </div>
       <UnorderedList list={comingSoonItems} listName="coming-soon" />
       <div className="content-page-paragraph">
-        {" "}
         At TMS we're always trying to improve our service and make your
         experience the best it can be. Look forward for more to come!
       </div>
