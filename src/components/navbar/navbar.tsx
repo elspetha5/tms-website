25a4af2dfee362709dbeaff7bb6167c2a63fe452
Elspeth Anderson <elspeth.anderson@discounttire.com>
2025-08-22 17:16:59 -0700

    Added spares info to page; started on request modal for spares

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 import { useAuth } from "../../shared/contexts/auth-context";
