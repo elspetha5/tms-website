f3b5f37744edda06f0f18517b2dc83bbbee21186
Elspeth Anderson <elspeth.anderson@discounttire.com>
2025-08-25 15:06:00 -0700

    started adding spares request form fields; added form script url;

--- src/library/modal/modal.tsx
+++ src/library/modal/modal.tsx
@@ -18,12 +18,13 @@ function Modal({ children, ...props }: PropsWithChildren<ModalProps>) {
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
-          width: 400,
+          width: "auto",
           bgcolor: "background.paper",
           border: "none",
           borderRadius: "5px",
           boxShadow: 24,
           p: 4,
+          scroll: "auto",
         }}
       >
         {children}
