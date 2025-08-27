3c1f543052f611a6941498711135d4fa660fa6ec
Elspeth Anderson <elspeth.anderson@discounttire.com>
2025-08-26 17:18:20 -0700

    Updated special for Fall

--- src/pages/get-started-page/promo-card/promo-card.tsx
+++ src/pages/get-started-page/promo-card/promo-card.tsx
@@ -3,11 +3,11 @@ import "./promo-card.scss";
 function PromoCard() {
   return (
     <div className="promo-card-container">
-      <div className="promo-card-title">*SUMMER SPECIAL*</div>
+      <div className="promo-card-title">*FALL SPECIAL*</div>
       <div className="promo-card-description">
         1st month free for new clients
       </div>
-      <div className="promo-card-date">June - August 2025</div>
+      <div className="promo-card-date">September - November 2025</div>
     </div>
   );
 }
